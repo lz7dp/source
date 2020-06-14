18d143e70a4f3bad991c982a61bab5051e135693
LZ7DP <lz7dp@yahoo.com>
2020-06-15 02:01:01 +0800

    14.06.2020

--- NodeMCU-ES8266/esppyser.py
+++ NodeMCU-ES8266/esppyser.py
@@ -1,12 +1,15 @@
 from machine import UART
+import time
 
-uart = UART(1, 115200)                         # init with given baudrate
+uart = UART(0, 115200)                         # init with given baudrate
 uart.init(115200, bits=8, parity=None, stop=1) # init with given parameters
 
-
-uart.read()
-uart.write('abc')
-
+#def myser():
+    #uart.read()
+    mybyte = 134
+    while TRUE
+        uart.write(mybyte)
+        time.sleep(100)
 
 
 
