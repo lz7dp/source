584cbb724126d9e7ef1f0bd7fa42a34624f2667f
LZ7DP <lz7dp@yahoo.com>
2020-06-14 21:06:22 +0300

    12.06.2020

--- NodeMCU-ES8266/new-test.py
+++ NodeMCU-ES8266/new-test.py
@@ -11,11 +11,13 @@ import uos, machine
 #uos.dupterm(uart, 1)
 
 from machine import Pin
-#pin4 = Pin(4, Pin.IN)
-#pin5 = Pin(5, Pin.OUT)
+pin13 = Pin(13, Pin.IN)
+pin15 = Pin(15, Pin.OUT)
 
 from machine import UART
-uart = UART(0, baudrate=115200)
-uart.write('hello')
-uart.read(5) # read up to 5 bytes
-
+#uart = UART(0, baudrate=115200)
+uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=pin15, rx=pin13)
+while TRUE:
+    uart.write('hello')
+    stt1 = uart.read(5) # read up to 5 bytes
+    print(str1)
