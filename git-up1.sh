3374f2afa128c73fa2a94e51a0b25c1481dc09cd
LZ7DP <lz7dp@yahoo.com>
2020-06-17 16:25:44 +0300

    17.06.2020

--- git-up1.sh
+++ git-up1.sh
@@ -5,3 +5,10 @@ git add *
 git commit -m "12.06.2020"
 git push origin HEAD:master
 
+echo "# certificate" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/lz7dp/certificate.git
+git push -u origin master
+
