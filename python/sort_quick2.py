e284b4e7d9a92eff060a8f2ca7759bb33b8cd397
LZ7DP <lz7dp@yahoo.com>
2021-11-22 17:25:57 +0200

    22.11.2021
    22.11.2021

---
+++ python/sort_quick2.py
@@ -0,0 +1,6 @@
+# qsort lambda algorythm  
+
+qsort = lambda l: l if len(l) <= 1 else qsort([x for x in l[1:] if x < l[0]]) + [l[0]] + qsort([x for x in l[1:] if x>= l[0]])
+
+print(qsort([17, 29, 11, 97, 103, 5]))
+
