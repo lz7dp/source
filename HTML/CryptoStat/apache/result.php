db3eadda6dd8828b06b053a4dab8a2a284222b45
LZ7DP <lz7dp@yahoo.com>
2022-01-17 11:43:40 +0200

    17.01.2022
    17.01.2022

---
+++ HTML/CryptoStat/apache/result.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$coin = $_POST['crypto'];
+$month2 = $_POST['month'];
+$year2 = $_POST['year'];
+
+echo "<h2>$coin coin statistic for month $month2 year $year2</h2>";
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>ID</th><th>DATE</th><th>TIME</th><th>$coin PRICE</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "cryptodb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    $stmt = $conn->prepare("SELECT * FROM $coin");
+    $stmt = $conn->prepare("SELECT * FROM $coin WHERE price_date  >= '$year2-$month2-01' AND price_date <= '$year2-$month2-31'");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+
+?>
+
+</body>
+</html>
+
---
+++ HTML/CryptoStat/apache/welcome2.php
@@ -0,0 +1,92 @@
+<?php
+require('includes/html_form.class.php');
+
+// arrays for select list demos
+$cryptos = array('BCN', 'BTC', 'STORJ', 'XRP', 'ETH', 'XRM', 'TNC');
+$months = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12); 
+$years = array(2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030); 
+
+// create instance of HTML_Form
+$frm = new HTML_Form();
+
+// using $frmStr to concatenate long string of form elements
+// startForm arguments: action, method, id, optional attributes added in associative array
+$frmStr = $frm->startForm('result.php', 'post', 'demoForm',
+            array('class'=>'demoForm', 'onsubmit'=>'return crypto month year') ) . PHP_EOL .
+    
+    // fieldset and legend elements
+    $frm->startTag('fieldset') . PHP_EOL .
+ 
+    $frm->startTag('legend') . 'Choice crypto & time period' . $frm->endTag() . PHP_EOL .
+    
+    // wrap form elements in paragraphs 
+    $frm->startTag('p') . 
+    // endTag remembers startTag (but you can pass tag if nesting or for clarity)
+    $frm->endTag() . PHP_EOL .
+
+    $frm->startTag('p') . 
+    $frm->startTag('label') . 'Crypto: ' . 
+    // arguments: name, array containing option text/values
+    // include values attributes (boolean),
+    // optional arguments: selected value, header, additional attributes in associative array
+    $frm->addSelectList('crypto', $cryptos, false, 'BCN') .
+    $frm->endTag('label') . 
+    $frm->endTag('p') . PHP_EOL .
+
+    $frm->startTag('p') .
+    $frm->startTag('label') . 'Month: ' .
+    // arguments: name, array of option values, array of option text
+    // optional arguments: selected value, header, additional attributes in associative array
+    $frm->addSelectList('month', $months, false, 8) .
+    $frm->endTag('label') .
+    $frm->endTag() . PHP_EOL .
+
+    $frm->startTag('p') .
+    $frm->startTag('label') . 'Year: ' .
+    // arguments: name, array of option values, array of option text
+    // optional arguments: selected value, header, additional attributes in associative array
+    $frm->addSelectList('year', $years, false, 2020) .
+    $frm->endTag('label') .
+    $frm->endTag() . PHP_EOL .
+
+    $frm->startTag('p') . 
+    $frm->addInput('submit', 'submit', 'Submit') .
+    $frm->endTag() . PHP_EOL .
+
+//    $frm->startTag('p') .
+//    $frm->addTextarea($name, $rows = 1, $cols = 100, $value = 'https://github.com/lz7dp/source/tree/master/CryptoStat' )
+//    $frm->endTag() . PHP_EOL .
+
+
+    $frm->endTag('legend') . PHP_EOL .
+    $frm->endTag('fieldset') . PHP_EOL .
+
+    $frm->endForm();
+
+// finally, output the long string
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+
+<body>
+
+<?php
+echo $frmStr;
+?>
+
+<a href="https://github.com/lz7dp/source/tree/master/CryptoStat">CryptoStat project source code</a>
+</body>
+</html>
+
+
