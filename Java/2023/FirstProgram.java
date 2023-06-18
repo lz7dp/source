9b1c98446fafb3b75f7ed98fd4f1003180fbaf49
lz7dp <66120628+lz7dp@users.noreply.github.com>
2023-06-18 23:19:23 +0300

    18.06.2023
    18.06.2023

---
+++ Java/2023/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+
+        public static void main(String[] args) {
+            System.out.println("hello");
+            System.out.println(42);
+            System.out.println("\n\tjava " + 16);
+        }
+}
---
+++ Java/2023/InputStringProgram.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputStringProgram {
+        public static void main(String[] args) {
+            Scanner scann = new Scanner(System.in);
+            String input = scann.next();
+            System.out.println("hello " + input);
+        }
+}
