df69ad7838ea4739db3e887c5b4e323e52e7cbf5
lz7dp <66120628+lz7dp@users.noreply.github.com>
2023-06-21 00:42:28 +0300

    21.06.2023
    21.06.2023

--- Java/2023/LearnArithmetic.java
+++ Java/2023/LearnArithmetic.java
@@ -19,19 +19,19 @@ public class LearnArithmetic {
         System.out.println("a -= b = " + (a -= b));
         System.out.println("a      = " + (a));
 		
-		int a = 60; 
-		int b = 13; 
+	int a = 60; 
+	int b = 13; 
 		 
-		System.out.println("a & b   = " + (a & b)); // 12  = ... 0000 1100
-		System.out.println("a | b   = " + (a | b)); // 61  = ... 0011 1101
-		System.out.println("a ^ b   = " + (a ^ b)); // 49  = ... 0011 0001
-		System.out.println("~a      = " + ~a); // -61 = 1111 1111 1111 1111 1111 1111 1100 0011
-		System.out.println("a << 2  = " + (a << 2)); // 240 = ... 1111 0000
-		System.out.println("a >> 2  = " + (a >> 2)); // 15  = ... 0000 1111
-		System.out.println("a >>> 2 = " + (a >>> 2)); // 15  = ... 0000 1111
+	System.out.println("a & b   = " + (a & b)); // 12  = ... 0000 1100
+	System.out.println("a | b   = " + (a | b)); // 61  = ... 0011 1101
+	System.out.println("a ^ b   = " + (a ^ b)); // 49  = ... 0011 0001
+	System.out.println("~a      = " + ~a); // -61 = 1111 1111 1111 1111 1111 1111 1100 0011
+	System.out.println("a << 2  = " + (a << 2)); // 240 = ... 1111 0000
+	System.out.println("a >> 2  = " + (a >> 2)); // 15  = ... 0000 1111
+	System.out.println("a >>> 2 = " + (a >>> 2)); // 15  = ... 0000 1111
 		
-		System.out.println(12345 + 5432l);
-		System.out.println("2 + 2 = " + 2 + 2);
-		System.out.println(010 | 4);
+	System.out.println(12345 + 5432l);
+	System.out.println("2 + 2 = " + 2 + 2);
+	System.out.println(010 | 4);
     }
 }
