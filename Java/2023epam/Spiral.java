53d3fe55dab0f87cbf4dbd01d7cdda8e92cad5dd
LZ7DP <lz7dp@yahoo.com>
2023-07-21 13:35:58 +0300

    21.07.2023
    21.07.2023

--- Java/2023epam/Spiral.java
+++ Java/2023epam/Spiral.java
@@ -1,54 +1,48 @@
-
 public class Spiral {
     public static void main(String[] args) {
-        int rows = 3;
-        int columns = 4;
-        int[][] spiralArray = spiral(rows, columns);
+        int[][] spiral = Spiral.spiral(4, 3);
 
-        // Print the spiral array
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.printf("%3d ", spiralArray[i][j]);
+        for (int i = 0; i < spiral.length; i++) {
+            for (int j = 0; j < spiral[i].length; j++) {
+                System.out.print(String.format("%4s", spiral[i][j]));
             }
             System.out.println();
         }
     }
 
     public static int[][] spiral(int rows, int columns) {
-        int[][] result = new int[rows][columns];
+        int[][] spiral = new int[rows][columns];
 
+        int top = 0, bottom = rows - 1, left = 0, right = columns - 1;
         int num = 1;
-        int topRow = 0;
-        int bottomRow = rows - 1;
-        int leftColumn = 0;
-        int rightColumn = columns - 1;
+        int totalElements = rows * columns;
 
-        while (num <= rows * columns) {
-            // Traverse from left to right along the top row
-            for (int i = leftColumn; i <= rightColumn; i++) {
-                result[topRow][i] = num++;
+        while (num <= totalElements) {
+            // Fill top row from left to right
+            for (int i = left; i <= right && num <= totalElements; i++) {
+                spiral[top][i] = num++;
             }
-            topRow++;
+            top++;
 
-            // Traverse from top to bottom along the rightmost column
-            for (int i = topRow; i <= bottomRow; i++) {
-                result[i][rightColumn] = num++;
+            // Fill right column from top to bottom
+            for (int i = top; i <= bottom && num <= totalElements; i++) {
+                spiral[i][right] = num++;
             }
-            rightColumn--;
+            right--;
 
-            // Traverse from right to left along the bottom row
-            for (int i = rightColumn; i >= leftColumn; i--) {
-                result[bottomRow][i] = num++;
+            // Fill bottom row from right to left
+            for (int i = right; i >= left && num <= totalElements; i--) {
+                spiral[bottom][i] = num++;
             }
-            bottomRow--;
+            bottom--;
 
-            // Traverse from bottom to top along the leftmost column
-            for (int i = bottomRow; i >= topRow; i--) {
-                result[i][leftColumn] = num++;
+            // Fill left column from bottom to top
+            for (int i = bottom; i >= top && num <= totalElements; i--) {
+                spiral[i][left] = num++;
             }
-            leftColumn++;
+            left++;
         }
 
-        return result;
+        return spiral;
     }
 }
