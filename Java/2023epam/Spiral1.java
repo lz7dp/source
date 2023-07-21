63b30df07a295264e66996965785102214515b9f
LZ7DP <lz7dp@yahoo.com>
2023-07-21 13:52:04 +0300

    21.07.2023
    21.07.2023

---
+++ Java/2023epam/Spiral1.java
@@ -0,0 +1,48 @@
+public class Spiral {
+    public static void main(String[] args) {
+        int[][] spiral = Spiral.spiral(3, 4);
+
+        for (int i = 0; i < spiral.length; i++) {
+            for (int j = 0; j < spiral[i].length; j++) {
+                System.out.print(String.format("%4s", spiral[i][j]));
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] spiral(int rows, int columns) {
+        int[][] spiralArr2 = new int[rows][columns];
+
+        int top = 0, bottom = rows - 1, left = 0, right = columns - 1;
+        int num = 1;
+        int totalElements = rows * columns;
+
+        while (num <= totalElements) {
+            // Fill top row from left to right
+            for (int i = left; i <= right && num <= totalElements; i++) {
+                spiralArr2[top][i] = num++;
+            }
+            top++;
+
+            // Fill right column from top to bottom
+            for (int i = top; i <= bottom && num <= totalElements; i++) {
+                spiralArr2[i][right] = num++;
+            }
+            right--;
+
+            // Fill bottom row from right to left
+            for (int i = right; i >= left && num <= totalElements; i--) {
+                spiralArr2[bottom][i] = num++;
+            }
+            bottom--;
+
+            // Fill left column from bottom to top
+            for (int i = bottom; i >= top && num <= totalElements; i--) {
+                spiralArr2[i][left] = num++;
+            }
+            left++;
+        }
+
+        return spiralArr2;
+    }
+}
