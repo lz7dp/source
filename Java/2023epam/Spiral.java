b23e178448d836ac15110a0a6311adf553963331
LZ7DP <lz7dp@yahoo.com>
2023-07-21 13:42:19 +0300

    21.07.2023
    21.07.2023

--- Java/2023epam/Spiral.java
+++ Java/2023epam/Spiral.java
@@ -1,17 +1,17 @@
 public class Spiral {
     public static void main(String[] args) {
-        int[][] spiral = Spiral.spiral(4, 3);
+        int[][] spiralArr = Spiral.spiral(4, 3);
 
-        for (int i = 0; i < spiral.length; i++) {
-            for (int j = 0; j < spiral[i].length; j++) {
-                System.out.print(String.format("%4s", spiral[i][j]));
+        for (int i = 0; i < spiralArr.length; i++) {
+            for (int j = 0; j < spiralArr[i].length; j++) {
+                System.out.print(String.format("%4s", spiralArr[i][j]));
             }
             System.out.println();
         }
     }
 
     public static int[][] spiral(int rows, int columns) {
-        int[][] spiral = new int[rows][columns];
+        int[][] spiralArr2 = new int[rows][columns];
 
         int top = 0, bottom = rows - 1, left = 0, right = columns - 1;
         int num = 1;
@@ -20,29 +20,29 @@ public class Spiral {
         while (num <= totalElements) {
             // Fill top row from left to right
             for (int i = left; i <= right && num <= totalElements; i++) {
-                spiral[top][i] = num++;
+                spiralArr2[top][i] = num++;
             }
             top++;
 
             // Fill right column from top to bottom
             for (int i = top; i <= bottom && num <= totalElements; i++) {
-                spiral[i][right] = num++;
+                spiralArr2[i][right] = num++;
             }
             right--;
 
             // Fill bottom row from right to left
             for (int i = right; i >= left && num <= totalElements; i--) {
-                spiral[bottom][i] = num++;
+                spiralArr2[bottom][i] = num++;
             }
             bottom--;
 
             // Fill left column from bottom to top
             for (int i = bottom; i >= top && num <= totalElements; i--) {
-                spiral[i][left] = num++;
+                spiralArr2[i][left] = num++;
             }
             left++;
         }
 
-        return spiral;
+        return spiralArr2;
     }
 }
