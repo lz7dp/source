73627eff646b4945832622e6b503401c53e939b4
LZ7DP <lz7dp@yahoo.com>
2023-08-21 15:43:47 +0300

    21.08.2023
    21.08.2023

---
+++ Java/2023epam/4. OOP/StartMethod.java
@@ -0,0 +1,13 @@
+class StartMethod {
+  public static void start(int number) {
+    System.out.println("Old value of \"number\" into \"start\" method is:" + number);
+    number = 3;
+    System.out.println("New value of \"number\" into \"start\" method is:" + number);
+  }
+  public static void main(String[] args) {
+    int number = 5;
+    System.out.println("Old value of \"number\" into \"main\" method is:" + number);
+    start(number);
+    System.out.println("New value of \"number\" into \"main\" method is:" + number);
+  }
+}
