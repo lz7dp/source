9e8eb878b2d1ce47245be2d9b9c6e4cda3d83e42
lz7dp <66120628+lz7dp@users.noreply.github.com>
2023-10-10 22:56:20 +0300

    10.10.2023
    10.10.2023

---
+++ Java/2023epam/4. OOP/InitDemo1.java
@@ -0,0 +1,18 @@
+public class InitDemo1 {
+    private static char ch;
+    private static boolean bb;
+    private static byte by;
+    private static int ii;
+    private static float ff;
+    private static String str;
+    private static int[] array;
+    public static void main(String[] arg) {
+        System.out.println("char: " + ch);
+        System.out.println("boolean: " + bb);
+        System.out.println("byte: " + by);
+        System.out.println("int: " + ii);
+        System.out.println("float: " + ff);
+        System.out.println("String: " + str);
+        System.out.println("int[]: " + array);
+    }
+}
